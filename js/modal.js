32a99c05e4394c17d5357554a5b5b41e7b26247d
SeregaGS <serega.gaskarov@rambler.ru>
2025-08-19 13:27:14 +0500

    Opening and closing modal, added universal functions, raw version

--- js/modal.js
+++ js/modal.js
@@ -1,48 +1,114 @@
-import { counterpartiesBalanceCurrency, clearBandgeContainer, renderCounterpartiesPaymentSeller } from './util.js';
+import {
+  counterpartiesBalanceCurrency,
+  clearBandgeContainer,
+  renderCounterpartiesPaymentSeller,
+  counterpartiesIsVerified,
+  renderBandgePayMethods
+} from './util.js';
+import { getFilteredTab } from './filter.js';
 
 const DOMElements = {
-  modalBuy: document.querySelector('.modal-buy')
+  formModalSell: document.querySelector('.modal-buy'),
+  formModalBuy: document.querySelector('.modal-sell'),
+  modalCloseBtn: document.querySelectorAll('.modal__close-btn'),
+  seller: document.querySelector('.modal.modal--buy'),
+  buyer: document.querySelector('.modal.modal--sell'),
+  tabsControls: document.querySelector('.tabs__controls'),
+};
+const SELECTORS = {
+  isVerify: '.transaction-info__data svg',
+  userName: '.transaction-info__item--name  .transaction-info__data',
+  exchangeRate: '.transaction-info__item--exchangerate .transaction-info__data',
+  balanceCurrency: '.transaction-info__item--cashlimit .transaction-info__data',
+  cardNumber: '[data-type="card-number"]',
+  cryptoNumber: '[data-type="crypto-number"]',
+  bandgeList: '.select select',
+  bandgeItem: 'option'
+};
+const PROVIDER = 'Cash in person';
+
+const cardNumberSellDefault = DOMElements.formModalSell.querySelector(SELECTORS.cardNumber).placeholder;
+const cardNumberBuyDefault = DOMElements.formModalBuy.querySelector(SELECTORS.cardNumber).placeholder;
+
+
+const paymentChange = (container, item, e) => {
+  const { paymentMethods } = item;
+  const paymentMethod = paymentMethods.find((prov) => e.target.value === prov.provider);
+  container.placeholder = (paymentMethod.provider === PROVIDER) ? '' : paymentMethod.accountNumber;
+};
+const isBuyModal = (type) => type === 'sell';
+const renderCounterpartiesModal = (modal, item, user, type) => {
+  const isBuyModal = type === 'sell';
+
+  modal.provider = isBuyModal ? item : user;
+  modal.querySelector(SELECTORS.userName).lastChild.textContent = item.userName;
+  modal.querySelector(SELECTORS.exchangeRate).textContent = `${item.exchangeRate} ₽`;
+  modal.querySelector(SELECTORS.balanceCurrency).textContent = counterpartiesBalanceCurrency(item);
+  modal.querySelector(SELECTORS.cryptoNumber).placeholder = isBuyModal ? user.wallet.address : item.wallet.address;
+  counterpartiesIsVerified(modal, item, SELECTORS.isVerify);
+  clearBandgeContainer(modal, SELECTORS.bandgeList, 1);
+  renderBandgePayMethods(modal, modal.provider, SELECTORS.bandgeList, '', SELECTORS.bandgeItem);
+};
+const renderCounterpartiesSell = (item, user) => {
+  DOMElements.formModalSell.provider = item;
+  DOMElements.formModalSell.querySelector(SELECTORS.userName).lastChild.textContent = item.userName;
+  DOMElements.formModalSell.querySelector(SELECTORS.exchangeRate).textContent = `${item.exchangeRate} ₽`;
+  DOMElements.formModalSell.querySelector(SELECTORS.balanceCurrency).textContent = counterpartiesBalanceCurrency(item);
+  DOMElements.formModalSell.querySelector(SELECTORS.cryptoNumber).placeholder = user.wallet.address;
+  counterpartiesIsVerified(DOMElements.formModalSell, item, SELECTORS.isVerify);
+  clearBandgeContainer(DOMElements.formModalSell, SELECTORS.bandgeList, 1);
+  renderCounterpartiesPaymentSeller(DOMElements.formModalSell, item, SELECTORS.bandgeList, '', SELECTORS.bandgeItem);
+};
+const renderCounterpartiesBuy = (item, user) => {
+  DOMElements.formModalBuy.provider = user;
+  DOMElements.formModalBuy.querySelector(SELECTORS.userName).lastChild.textContent = item.userName;
+  DOMElements.formModalBuy.querySelector(SELECTORS.exchangeRate).textContent = `${item.exchangeRate} ₽`;
+  DOMElements.formModalBuy.querySelector(SELECTORS.balanceCurrency).textContent = counterpartiesBalanceCurrency(item);
+  DOMElements.formModalBuy.querySelector(SELECTORS.cryptoNumber).placeholder = item.wallet.address;
+  counterpartiesIsVerified(DOMElements.formModalBuy, item, SELECTORS.isVerify);
+  clearBandgeContainer(DOMElements.formModalBuy, SELECTORS.bandgeList, 1);
+  renderBandgePayMethods(DOMElements.formModalBuy, user, SELECTORS.bandgeList, '', SELECTORS.bandgeItem);
 };
 
-const customInput = DOMElements.modalBuy.querySelector('.modal__input-wrapper--decorated input');
-const modalCloseBtn = document.querySelector('.modal__close-btn');
-const originalPlaceholder = customInput.placeholder;
-
-const renderModalCounterparties = (item) => {
-  DOMElements.modalBuy.querySelector('.transaction-info__data').lastChild.textContent = item.userName;
-  DOMElements.modalBuy.querySelector('.transaction-info__item--exchangerate .transaction-info__data').textContent = `${item.exchangeRate} ₽`;
-  DOMElements.modalBuy.querySelector('.transaction-info__item--cashlimit .transaction-info__data').textContent = counterpartiesBalanceCurrency(item);
-  clearBandgeContainer(DOMElements.modalBuy, '.select select', 1);
-  renderCounterpartiesPaymentSeller(DOMElements.modalBuy, item, '.select select', '', 'option');
-  const select = DOMElements.modalBuy.querySelector('.select select');
-  select.addEventListener('change', (e) => {
-    const { paymentMethods } = item;
-    const paymentMethod = paymentMethods.find((prov) => e.target.value === prov.provider);
-    customInput.placeholder = (paymentMethod.provider === 'Cash in person') ? '' : paymentMethod.accountNumber;
-  });
-  const input = DOMElements.modalBuy.querySelector('[data-payment="pay"]');
-  const result = DOMElements.modalBuy.querySelector('[data-payment="crypto"]');
-  input.addEventListener('input', () => {
-    result.value = `${(Number(input.value) / item.exchangeRate).toFixed(2)}`;
-  });
-  result.addEventListener('input', () => {
-    input.value = `${(Number(result.value) * item.exchangeRate).toFixed(2)}`;
-  });
+const renderCounterpartiesAll = (item, user) => {
+  if (getFilteredTab(DOMElements.tabsControls) === 'seller') {
+    renderCounterpartiesModal(DOMElements.formModalSell, item, user, 'sell');
+  } else {
+    renderCounterpartiesModal(DOMElements.formModalBuy, item, user, 'buy');
+  }
 };
-const openModalBuy = () => {
-  const modal = document.querySelector('.modal.modal--buy');
+const openModal = (type) => {
+  const modal = DOMElements[type];
   modal.style.display = 'block';
-
 };
 
-const closeModalBuy = () => {
-  const modal = document.querySelector('.modal.modal--buy');
-  modal.style.display = 'none';
-  customInput.placeholder = originalPlaceholder;
-  DOMElements.modalBuy.reset();
+const closeModal = (button) => {
+  button.style.display = 'none';
+  DOMElements.formModalBuy.reset();
+  DOMElements.formModalSell.reset();
+  DOMElements.formModalSell.querySelector(SELECTORS.cardNumber).placeholder = cardNumberSellDefault;
+  DOMElements.formModalBuy.querySelector(SELECTORS.cardNumber).placeholder = cardNumberBuyDefault;
 };
-modalCloseBtn.addEventListener('click', () => {
-  closeModalBuy();
+
+DOMElements.modalCloseBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const modal = btn.closest('.modal');
+    closeModal(modal);
+  });
 });
 
-export { renderModalCounterparties, openModalBuy };
+DOMElements.formModalSell.addEventListener('change', (e) => {
+  const select = DOMElements.formModalSell.querySelector('.select select');
+  if (e.target === select) {
+    const item = DOMElements.formModalSell.provider;
+    paymentChange(DOMElements.formModalSell.querySelector(SELECTORS.cardNumber), item, e);
+  }
+});
+DOMElements.formModalBuy.addEventListener('change', (e) => {
+  const select = DOMElements.formModalBuy.querySelector('.select select');
+  if (e.target === select) {
+    const item = DOMElements.formModalBuy.provider;
+    paymentChange(DOMElements.formModalBuy.querySelector(SELECTORS.cardNumber), item, e);
+  }
+});
+export { openModal, renderCounterpartiesAll };
