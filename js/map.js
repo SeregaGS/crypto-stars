db7ca7434cb0125264c42d6b050a4b505d0ed16b
SeregaGS <serega.gaskarov@rambler.ru>
2025-08-21 14:25:12 +0500

    Fixing imported modules

--- js/map.js
+++ js/map.js
@@ -1,10 +1,20 @@
-import { clearBandgeContainer, renderCounterpartiesPaymentSeller, counterpartiesIsVerified, counterpartiesBalanceCurrency } from './util.js';
-
+import {
+  clearBandgeContainer,
+  renderBandgePayMethods,
+  counterpartiesIsVerified,
+  counterpartiesBalanceCurrency
+} from './util.js';
+import { getFilteredTab } from './filter.js';
+import { openModal } from './modal.js';
 
 const TILE_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const ATTRIBUTION = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>';
 const PROVIDER = 'Cash in person';
 
+const DOMElements = {
+  tabsControls: document.querySelector('.tabs__controls'),
+  templateBaloon: document.querySelector('#map-baloon__template')
+};
 const MAP_COORDINATES_DEFAULT = {
   lat: 59.92749,
   lng: 30.31127,
@@ -19,21 +29,25 @@ const SELECTORS = {
   exchangeRate: '[data-cash="exchange-rate"]',
   limit: '[data-cash="limit"]',
   badgesList: '.user-card__badges-list',
-  badgesItem: ['users-list__badges-item', 'badge']
+  badgesItem: ['users-list__badges-item', 'badge'],
+  btnOpenModal: '.btn--green'
 };
 
 // Создаём DOM-элемент балуна (всплывающего окна) для маркера на карте
-const mapPinBalloon = (counterparty) => {
+const mapPinBalloon = (counterparty, user) => {
   const { userName, balance, exchangeRate } = counterparty;
-  const balloonTemplate = document.querySelector('#map-baloon__template').content.querySelector(SELECTORS.counterpartyCard);
+  const balloonTemplate = DOMElements.templateBaloon.content.querySelector(SELECTORS.counterpartyCard);
   const balloonElement = balloonTemplate.cloneNode(true);
   balloonElement.querySelector(SELECTORS.counterpartyName).textContent = userName;
   balloonElement.querySelector(SELECTORS.currency).textContent = balance.currency;
   balloonElement.querySelector(SELECTORS.exchangeRate).textContent = exchangeRate.toLocaleString('ru-RU', { style: 'currency', currency: 'RUB' });
   balloonElement.querySelector(SELECTORS.limit).textContent = counterpartiesBalanceCurrency(counterparty);
   clearBandgeContainer(balloonElement, SELECTORS.badgesList);
-  renderCounterpartiesPaymentSeller(balloonElement, counterparty, SELECTORS.badgesList, SELECTORS.badgesItem);
+  renderBandgePayMethods(balloonElement, counterparty, SELECTORS.badgesList, SELECTORS.badgesItem);
   counterpartiesIsVerified(balloonElement, counterparty, SELECTORS.counterpartyCardSvg);
+  balloonElement.querySelector(SELECTORS.btnOpenModal).addEventListener('click', () => {
+    openModal(getFilteredTab(DOMElements.tabsControls));
+  });
   return balloonElement;
 };
 // Возвращаем иконку маркера в зависимости от статуса верификации.
