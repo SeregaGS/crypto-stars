0242317ae846559932d7c86a1be7d852cb46dd9e
SeregaGS <serega.gaskarov@rambler.ru>
2025-08-22 12:06:44 +0500

    optimization of rendering of the list of counterparties, improved readability and reusability of the code

--- js/counterparties.js
+++ js/counterparties.js
@@ -19,22 +19,28 @@ const SELECTORS = {
   exchangeRate: '.users-list__table-exchangerate',
   limit: '.users-list__table-cashlimit',
   isVerify: '.users-list__table-name svg',
-  badgesList: '.users-list__badges-list',
-  badgesItem: ['users-list__badges-item', 'badge'],
+  bandgeList: '.users-list__badges-list',
+  bandgeItem: ['users-list__badges-item', 'badge'],
   button: '.btn--greenborder'
 };
 
-const createCounterpartiesItem = (counterparty, user) => {
+const renderCounterpartyInfo = (container, counterparty) => {
   const { userName, balance, exchangeRate, id } = counterparty;
+  container.querySelector(SELECTORS.containerUser).id = id;
+  container.querySelector(SELECTORS.name).textContent = userName;
+  container.querySelector(SELECTORS.currency).textContent = balance.currency;
+  container.querySelector(SELECTORS.exchangeRate).textContent = `${exchangeRate} ₽`;
+  container.querySelector(SELECTORS.limit).textContent = counterpartiesBalanceCurrency(counterparty);
+  counterpartiesIsVerified(container, counterparty, SELECTORS.isVerify);
+};
+const renderModalPaymentMethodsSeller = (modal, counterparty) => {
+  clearBandgeContainer(modal, SELECTORS.bandgeList);
+  renderSellerPaymentBadges(modal, counterparty, SELECTORS.bandgeList, SELECTORS.bandgeItem);
+};
+const createCounterpartiesItem = (counterparty, user) => {
   const list = DOMElements.userTableRowTemplate.cloneNode(true);
-  list.querySelector(SELECTORS.containerUser).id = id;
-  list.querySelector(SELECTORS.name).textContent = userName;
-  list.querySelector(SELECTORS.currency).textContent = balance.currency;
-  list.querySelector(SELECTORS.exchangeRate).textContent = `${exchangeRate} ₽`;
-  list.querySelector(SELECTORS.limit).textContent = counterpartiesBalanceCurrency(counterparty);
-  counterpartiesIsVerified(list, counterparty, SELECTORS.isVerify);
-  clearBandgeContainer(list, SELECTORS.badgesList);
-  renderSellerPaymentBadges(list, counterparty, SELECTORS.badgesList, SELECTORS.badgesItem);
+  renderCounterpartyInfo(list, counterparty);
+  renderModalPaymentMethodsSeller(list, counterparty);
   list.querySelector(SELECTORS.button).addEventListener('click', () => {
     openModal(getFilteredTab(DOMElements.tabsControls));
     renderCounterpartiesAll(counterparty, user);
@@ -44,8 +50,8 @@ const createCounterpartiesItem = (counterparty, user) => {
 const renderCounterparties = (data, user) => {
   DOMElements.usersListContainer.replaceChildren();
   const fragment = document.createDocumentFragment();
-  data.forEach((item) => {
-    const createCounterparties = createCounterpartiesItem(item, user);
+  data.forEach((counterparty) => {
+    const createCounterparties = createCounterpartiesItem(counterparty, user);
     fragment.append(createCounterparties);
   });
   DOMElements.usersListContainer.append(fragment);
