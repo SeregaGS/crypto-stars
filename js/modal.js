33f2dcae78f91c5ae15faed37dcc7b35ecd00729
SeregaGS <serega.gaskarov@rambler.ru>
2025-08-28 11:27:51 +0500

    Min and max refinement, added the ability to have a 'Exchange all' button, added closing the modal by overlay and pressing esc, minor refactoring

--- js/modal.js
+++ js/modal.js
@@ -8,6 +8,7 @@ import { getFilteredTab } from './filter.js';
 
 const DOMElements = {
   body: document.querySelector('body'),
+  modal: document.querySelectorAll('.modal'),
   formModalSell: document.querySelector('.modal-buy'),
   formModalBuy: document.querySelector('.modal-sell'),
   modalCloseBtn: document.querySelectorAll('.modal__close-btn'),
@@ -43,26 +44,23 @@ const cardNumberSellDefault = DOMElements.formModalSell.querySelector(SELECTORS.
 const cardNumberBuyDefault = DOMElements.formModalBuy.querySelector(SELECTORS.cardNumber).placeholder;
 
 // Выбор платежной системы и обновление placeholder
-// Если выбран Cash in person, поле пустое, иначе показываем номер счета
 const updatePaymentPlaceholder = (container, counterparty, event) => {
   const { paymentMethods } = counterparty;
   const selectedMethod = paymentMethods.find((prov) => event.target.value === prov.provider);
   container.placeholder = (selectedMethod.provider === PROVIDER) ? '' : selectedMethod.accountNumber;
 };
 // Обновление placeholder для крипто-кошелька
-// Если покупка — показываем кошелек пользователя, если продажа — кошелек контрагента
 const updateCryptoWallet = (modalElement, isBuyMode, user, counterparty) => {
   modalElement.querySelector(SELECTORS.cryptoNumber).placeholder = isBuyMode
     ? user.wallet.address
     : counterparty.wallet.address;
 };
-// Рендер платежных систем
+// Очищаем и рендерим платежные системы
 const renderModalPaymentMethods = (modalElement, provider) => {
   clearBandgeContainer(modalElement, SELECTORS.bandgeList, 1);
   renderBandgePayMethods(modalElement, provider, SELECTORS.bandgeList, '', SELECTORS.bandgeItem);
 };
 // Отображаем информацию о контрагенте
-// Показываем имя, курс обмена и доступный баланс
 const renderCounterpartyInfo = (modalElement, counterparty) => {
   modalElement.querySelector(SELECTORS.userName).lastChild.textContent = counterparty.userName;
   modalElement.querySelector(SELECTORS.exchangeRate).textContent = `${counterparty.exchangeRate} ${CURRENCY.RUB}`;
@@ -82,8 +80,7 @@ const getUserBalance = (user, mode) => {
 const assignContainerMax = (container, first, second) => {
   container.max = Math.min(first, second);
 };
-
-// Минимальные суммы для фиата и крипты
+// Минимальные суммы для фиата и крипты в input min
 const updateMinAmounts = (fiatMin, cryptoMin, counterparty, user, mode) => {
   const fiatMinAmount = mode === 'sell'
     ? counterparty.minAmount * counterparty.exchangeRate
@@ -94,29 +91,35 @@ const updateMinAmounts = (fiatMin, cryptoMin, counterparty, user, mode) => {
     : counterparty.minAmount / counterparty.exchangeRate;
   cryptoMin.min = Math.min(roundingAmount(cryptoMinAmount), getUserBalance(user, 'buy'));
 };
-// Максимальная сумма сделки для фиата
+// Максимальная сумма сделки для фиата в input max
 const updateMaxFiatAmount = (container, counterparty, user, mode) => {
-  const maxAmount = mode === 'sell' ? counterparty.balance.amount * counterparty.exchangeRate : getUserBalance(user, mode) * counterparty.exchangeRate;
-  const counterpartyAvailableAmount = maxAmount ? roundingAmount(maxAmount) : 0;
+  const maxAmount = mode === 'sell'
+    ? counterparty.balance.amount * counterparty.exchangeRate
+    : getUserBalance(user, mode) * counterparty.exchangeRate;
   const convertedUserBalance = getUserBalance(user, 'sell');
-  assignContainerMax(container, counterpartyAvailableAmount, convertedUserBalance);
+  assignContainerMax(container, roundingAmount(maxAmount), convertedUserBalance);
 };
-// Максимальная сумма сделки для крипты
+// Максимальная сумма сделки для крипты в input max
 const updateMaxCryptoAmount = (container, counterparty, user, mode) => {
-  const maxAmount = mode === 'sell' ? counterparty.balance.amount : counterparty.balance.amount / counterparty.exchangeRate;
-  const counterpartyAvailableAmount = maxAmount ? roundingAmount(maxAmount) : 0;
-  const convertedUserBalance = mode === 'sell' ? roundingAmount(getUserBalance(user, mode) / counterparty.exchangeRate) : getUserBalance(user, mode);
-  assignContainerMax(container, counterpartyAvailableAmount, convertedUserBalance);
+  const maxAmount = mode === 'sell'
+    ? counterparty.balance.amount
+    : counterparty.balance.amount / counterparty.exchangeRate;
+  const convertedUserBalance = mode === 'sell'
+    ? roundingAmount(getUserBalance(user, mode) / counterparty.exchangeRate)
+    : getUserBalance(user, mode);
+  assignContainerMax(container, roundingAmount(maxAmount), convertedUserBalance);
 };
 // Обновляем минимальные и максимальные значения одновременно
-const updateMinMaxAmounts = (fiatInput, cryptoInput, counterparty, user, mode) => {
-  updateMinAmounts(fiatInput, cryptoInput, counterparty, user, mode);
-  updateMaxCryptoAmount(cryptoInput, counterparty, user, mode);
-  updateMaxFiatAmount(fiatInput, counterparty, user, mode);
-};
+const updateMinMaxAmounts = (modalElement, counterparty, user, mode) => {
+  const dataPaymentFiat = modalElement.querySelector(SELECTORS.dataPaymentFiat);
+  const dataPaymentCrypto = modalElement.querySelector(SELECTORS.dataPaymentCrypto);
 
+  updateMinAmounts(dataPaymentFiat, dataPaymentCrypto, counterparty, user, mode);
+  updateMaxCryptoAmount(dataPaymentCrypto, counterparty, user, mode);
+  updateMaxFiatAmount(dataPaymentFiat, counterparty, user, mode);
+};
 // Делегирование input в максимум при нажатии кнопки
-const setMaxOnClicks = (modalElement, counterparty) => {
+const handleMaxButtonClicks = (modalElement, counterparty) => {
   const dataPaymentFiat = modalElement.querySelector(SELECTORS.dataPaymentFiat);
   const dataPaymentCrypto = modalElement.querySelector(SELECTORS.dataPaymentCrypto);
 
@@ -132,7 +135,7 @@ const setMaxOnClicks = (modalElement, counterparty) => {
   });
 };
 // Делегирование ввода фиата и крипты
-const changeInputValues = (modalElement, counterparty) => {
+const syncInputValues = (modalElement, counterparty) => {
   const dataPaymentFiat = modalElement.querySelector(SELECTORS.dataPaymentFiat);
   const dataPaymentCrypto = modalElement.querySelector(SELECTORS.dataPaymentCrypto);
 
@@ -146,28 +149,27 @@ const changeInputValues = (modalElement, counterparty) => {
   });
 };
 // Очистка input при закрытии модалки
-const cleanInputValue = (fiatInput, cryptoInput) => {
-  fiatInput.value = '';
-  cryptoInput.value = '';
+const cleanInputValue = (modalElement) => {
+  const inputs = `${SELECTORS.dataPaymentFiat}, ${SELECTORS.dataPaymentCrypto}`;
+  const valueInputs = modalElement.querySelectorAll(inputs);
+  valueInputs.forEach((input) => {
+    input.value = '';
+  });
 };
-
 // Основной рендер модальнего окна
 const renderCounterpartiesModal = (modalElement, counterparty, user, mode) => {
   const isBuyModal = mode === 'sell';
   modalElement.provider = isBuyModal ? counterparty : user;
-  const dataPaymentFiat = modalElement.querySelector(SELECTORS.dataPaymentFiat);
-  const dataPaymentCrypto = modalElement.querySelector(SELECTORS.dataPaymentCrypto);
+
   renderCounterpartyInfo(modalElement, counterparty);
   updateCryptoWallet(modalElement, isBuyModal, user, counterparty);
   counterpartiesIsVerified(modalElement, counterparty, SELECTORS.isVerify);
   renderModalPaymentMethods(modalElement, modalElement.provider);
-  updateMinMaxAmounts(dataPaymentFiat, dataPaymentCrypto, counterparty, user, mode);
-  changeInputValues(modalElement, counterparty);
-  setMaxOnClicks(modalElement, counterparty);
-  // Обменять всё * РУБЛИ * и * КРИПТО *
-  // setMaxOnClick(modalElement.querySelector(SELECTORS.exchangeFiatButton), dataPaymentFiat, dataPaymentCrypto, counterparty);
-  // setMaxOnClick(modalElement.querySelector(SELECTORS.exchangeCryptoButton), dataPaymentCrypto, dataPaymentFiat, counterparty);
 
+  updateMinMaxAmounts(modalElement, counterparty, user, mode);
+
+  syncInputValues(modalElement, counterparty);
+  handleMaxButtonClicks(modalElement, counterparty);
 };
 
 // Рендерим всех контрагентов в зависимости от выбранной вкладки
@@ -178,27 +180,39 @@ const renderCounterpartiesAll = (counterparty, user) => {
     renderCounterpartiesModal(DOMElements.formModalBuy, counterparty, user, 'buy');
   }
 };
-
-
 const openModal = (mode) => {
   DOMElements[mode].style.display = 'block';
   DOMElements.body.classList.add('scroll-lock');
+
+  const escEvent = (event) => {
+    if (event.key === 'Escape') {
+      closeModal(DOMElements[mode]);
+      document.removeEventListener('keydown', escEvent);
+    }
+  };
+  document.addEventListener('keydown', escEvent);
 };
 const closeModal = (modalElement) => {
   modalElement.style.display = 'none';
   DOMElements.body.classList.remove('scroll-lock');
   const isModalSell = modalElement.querySelector(SELECTORS.form);
   isModalSell.reset();
+
   const isSell = isModalSell.classList.contains('modal-buy');
-  isModalSell.querySelector(SELECTORS.cardNumber).placeholder = isSell ? cardNumberSellDefault : cardNumberBuyDefault;
 
+  isModalSell.querySelector(SELECTORS.cardNumber).placeholder = isSell
+    ? cardNumberSellDefault
+    : cardNumberBuyDefault;
 
-  cleanInputValue(isModalSell.querySelector(SELECTORS.dataPaymentFiat), isModalSell.querySelector(SELECTORS.dataPaymentCrypto));
+  cleanInputValue(modalElement);
 };
-const modalCloseBtns = (closeButtons) => {
-  closeButtons.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      const modal = btn.closest(SELECTORS.modal);
+
+const modalCloseBtn = (modalElements) => {
+  modalElements.forEach((modal) => {
+    modal.addEventListener('click', (e) => {
+      const overlayModal = e.target.closest('.modal__overlay');
+      const closeButton = e.target.closest('.modal__close-btn');
+      if (!overlayModal && !closeButton) { return; }
       closeModal(modal);
     });
   });
@@ -213,8 +227,9 @@ const attachPaymentChange = (formModal) => {
   });
 };
 
-modalCloseBtns(DOMElements.modalCloseBtn);
+modalCloseBtn(DOMElements.modal);
 attachPaymentChange(DOMElements.formModalSell);
 attachPaymentChange(DOMElements.formModalBuy);
+
 
 export { openModal, renderCounterpartiesAll, renderCounterpartiesModal };
